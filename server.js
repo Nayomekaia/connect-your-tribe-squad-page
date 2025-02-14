de1c07319a2ca57364568e807b3e885d84ddf2ef
Nayomekaia <nayomedoelwijt@gmail.com>
2025-02-14 09:49:52 +0100

    server.js filter roegevoegd

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ const squadResponse = await fetch('https://fdnd.directus.app/items/squad?filter=
 // Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
 const squadResponseJSON = await squadResponse.json()
 
-// Controleer de data in je console (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
-// console.log(squadResponseJSON)
-
-
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
@@ -27,27 +23,40 @@ const engine = new Liquid();
 app.engine('liquid', engine.express()); 
 
 // Stel de map met Liquid templates in
-// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set('views', './views')
 
 // Zorg dat werken met request data makkelijker wordt
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // Om Views weer te geven, heb je Routes nodig
 // Maak een GET route voor de index
 app.get('/', async function (request, response) {
   // Haal alle personen uit de WHOIS API op, van dit jaar
-  const personResponse = await fetch('https://fdnd.directus.app/items/person/?fields=*,squads.squad_id.name,squads.squad_id.cohort&filter={"_and":[{"squads":{"squad_id":{"tribe":{"name":"FDND Jaar 1"}}}},{"squads":{"squad_id":{"cohort":"2425"}}}]}')
-  // En haal daarvan de JSON op
-  const personResponseJSON = await personResponse.json()
-  
-  // personResponseJSON bevat gegevens van alle personen uit alle squads van dit jaar
-  // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
+  const personResponse = await fetch('https://fdnd.directus.app/items/person/?sort=birthdate&fields=*,squads.squad_id.name,squads.squad_id.cohort&filter={"_and":[{"squads":{"squad_id":{"tribe":{"name":"FDND Jaar 1"}}}},{"squads":{"squad_id":{"cohort":"2425"}}}]}');
 
-  // Render index.liquid uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
-  // Geef ook de eerder opgehaalde squad data mee aan de view
-  response.render('index.liquid', {persons: personResponseJSON.data, squads: squadResponseJSON.data})
-})
+  const personResponseJSON = await personResponse.json();
+
+  // Haal het geselecteerde geboortejaar uit de query (bijvoorbeeld: ?year=2003)
+  const year = request.query.year;
+
+  // Filter de personen op geboortejaar (indien een jaar is opgegeven)
+  let filteredPersons = personResponseJSON.data;
+
+  if (year) {
+    filteredPersons = filteredPersons.filter(person => {
+      const birthYear = new Date(person.birthdate).getFullYear().toString(); // Haal jaar uit de geboortedatum
+      return birthYear === year; // Vergelijk geboortejaar met geselecteerd jaar
+    });
+  }
+
+  // Render de pagina met de gefilterde personen en geef ze door aan de Liquid template
+  response.render('index.liquid', {
+    persons: filteredPersons,
+    squads: squadResponseJSON.data,
+    selectedYear: year // Stuur het geselecteerde jaar mee naar Liquid
+  });
+});
+
 
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 app.post('/', async function (request, response) {
